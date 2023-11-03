b22a78b9e39252943aa949fa8c91d2e2efafe99b
feng <78236491@qq.com>
2023-11-03 11:39:09 +0800

    组件化和基本包搭建完成,尚未实现组件自启动

---
+++ app/src/main/java/com/fhz/luban/LuBanApp.kt
@@ -0,0 +1,17 @@
+package com.fhz.luban
+
+import android.app.Application
+
+/**
+ * 时间:2023/11/2
+ * @author Mr.Feng
+ * 简述: 启动app
+ */
+
+class LuBanApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ buildSrc/src/main/kotlin/AndroidX.kt
@@ -0,0 +1,17 @@
+object AndroidX {
+    val corKktx  = "androidx.core:core-ktx:1.9.0"
+    val appcompat = "androidx.appcompat:appcompat:1.6.1"
+    val material =  "com.google.android.material:material:1.8.0"
+    val junit = "junit:junit:4.13.2"
+    val TestImplementation = "androidx.test.ext:junit:1.1.5"
+    val espresso = "androidx.test.espresso:espresso-core:3.5.1"
+    val compileSdk = 33
+    val minSdk = 26
+    val targetSdk = 33
+    val versionCode = 1
+    val versionName = "1.0"
+}
+
+object Dependencies{
+    val gson = "com.google.code.gson:gson:2.8.6"
+}

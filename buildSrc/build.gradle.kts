8560bbf561e7f58d5e56869949aa86d471124411
feng <78236491@qq.com>
2023-11-06 09:42:52 +0800

    kotlin buildsrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,3 +7,12 @@ repositories {
     google()
     gradlePluginPortal()
 }
+
+kotlin {
+    version = 1.8
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}

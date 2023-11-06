412bbd7572af9290bea663341f3f44a73fab8e2b
feng <78236491@qq.com>
2023-11-06 10:35:59 +0800

    hlit集成成功

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,20 +1,33 @@
 plugins {
     `kotlin-dsl`
 }
-
 repositories {
 //    maven{url = uri("https://xxxxxxxxx")}
-
     mavenCentral()
     google()
     gradlePluginPortal()
+
 }
+
+
+tasks.compileKotlin {
+    kotlinOptions {
+        jvmTarget = "1.8"
+//        useIR = true
+    }
+}
+
+tasks.compileTestKotlin {
+    kotlinOptions {
+        jvmTarget = "1.8"
+//        useIR = true
+    }
+}
+
+
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-kotlin {
-    version = "1.8"
-}
---
+++ buildSrc/src/main/kotlin/Example.kt
@@ -0,0 +1,10 @@
+import org.gradle.kotlin.dsl.KotlinBuildScript
+
+//fun KotlinBuildScript.configureModule() {
+//    tasks.named<KotlinCompile>("compileKotlin") {
+//        kotlinOptions {
+//            jvmTarget = "1.8"
+//            useIR = true
+//        }
+//    }
+//}
